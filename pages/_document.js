e9e760dfd6b45ba9540be076a0408406824909f5
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-02-23 14:57:11 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin="true"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,200..1000;1,200..1000&display=swap"
           rel="stylesheet"
         />
       </Head>
